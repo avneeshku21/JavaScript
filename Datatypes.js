c321634ca2fcd1637cdbbd604e1a95c7433b2791
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-07 22:48:26 +0530

    neew

--- Datatypes.js
+++ Datatypes.js
@@ -1,1 +1,21 @@
+//DataTypes
+// 1 Number
+// 2 String 
+// 3 Boolean
+// 4 undefined
+// 5 null
+// 6 BigInt
+// 7 Symbol
+
+
+
 console.log(typeof(7))
+console.log(typeof("Hello")) 
+console.log(typeof(~6))// number
+console.log(typeof(+'avneesh'))// NaN
+console.log(parseInt('1010hsh'))// only give number
+
+// How to convert number to string
+console.log(typeof(100+''))// string
+// How to convert string to Number
+console.log(typeof(+"avneesh"))// number
