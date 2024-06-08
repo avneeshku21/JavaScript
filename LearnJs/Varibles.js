c85cd63e2124749f0c68debf0867dbf72d1e7713
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-08 10:40:14 +0530

    New

--- LearnJs/Varibles.js
+++ LearnJs/Varibles.js
@@ -11,7 +11,7 @@ let age =21
 firstname='Avii' // Overwrite
 
 let userIntro='Hey I am ' +firstname+' '+lastname+' ' +'. I am '+age+' ' +'Year old'
-console.log(userIntro)
+//console.log(userIntro)
 
 
 
@@ -20,8 +20,19 @@ console.log(userIntro)
 let a=undefined
 //console.log(a)
 let b=null
-console.log(typeof(b))
+//console.log(typeof(b))
 
+/*************Const********************** */
+// Ham kisi bhi Varibale ko change nhi kr skte
+// const key me hame intialize krna padta hai
+const hoursInDay=24
+//hoursInDay=24
+//console.log(hoursInDay)
+
+/**********************Var****************** */
+// It is used old Method
+// Var ko bhi dobara declear kr skte hai
+// var keyword ko ham block of code k bahar se bhi Access krte  Sakte hai
 
 
 
