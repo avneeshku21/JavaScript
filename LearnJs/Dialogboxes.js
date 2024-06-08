880f1d485c8655e73290080ee0f17868293c59ab
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-08 15:53:16 +0530

    new

---
+++ LearnJs/Dialogboxes.js
@@ -0,0 +1,13 @@
+
+/****************Dialog Boxes*********************
+ 
+1 alert("Your message goes here")
+
+2 confirm("Your message goes here")
+
+3 prompt("Your message goes Here")
+
+ */
+alert('hello kaise ho ap log')
+confirm('Ap hi ho ya koi aur')
+prompt("To kaise hai ap log")
