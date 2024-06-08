5dfcb6bb41a827496b92e18267b3e5a63e8d630d
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-08 15:37:54 +0530

    new

---
+++ LearnJs/lineCode.js
@@ -0,0 +1,24 @@
+// Line by Line code execution
+
+/*****************Important Point***************
+ debugger-: debugging tool is a computer program used to test and debug other programs 
+
+
+
+ deffer-:your script will only execute after the page has finished loading.
+
+<script src="demo_defer.js" defer></script>
+
+
+
+* */
+//console.log(typeof(firstname))// it gives Undefiend
+
+//console.log(typeof(typeof(firstname)))
+
+var firstname='Avneesh'
+let lastname='Kumar'
+let age=15
+const yearOfbirth=2003
+
+//let userIntro='Hi, my name is '+firstname+' '+' '+ lastname
