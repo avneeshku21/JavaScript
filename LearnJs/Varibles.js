af283acc314b31034bdf02235c32e0010e7461f3
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-08 10:30:42 +0530

    new

--- LearnJs/Varibles.js
+++ LearnJs/Varibles.js
@@ -8,7 +8,20 @@
 let firstname="Avneesh"
 let lastname='Kumar'
 let age =21
-console.log(age)
+firstname='Avii' // Overwrite
 
-let userIntro='Hey I am ' +firstname+' '+lastname+' ' +' I am '+age+' ' +'old'
+let userIntro='Hey I am ' +firstname+' '+lastname+' ' +'. I am '+age+' ' +'Year old'
 console.log(userIntro)
+
+
+
+
+//Undefiend
+let a=undefined
+//console.log(a)
+let b=null
+console.log(typeof(b))
+
+
+
+
