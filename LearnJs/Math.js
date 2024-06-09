9884eee09be0b4cd78d84666c5dcd1ba5be8f405
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-09 12:43:02 +0530

    new

--- LearnJs/Math.js
+++ LearnJs/Math.js
@@ -40,8 +40,9 @@ function getRoot2() {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  
+
   
   const rndInt = randomIntFromInterval(10, 20);
   console.log(rndInt);
 
+console.log(typeof(Infinity))
