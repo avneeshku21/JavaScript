0469000cd3c3fa69e1a27c0374e4cd94ba76f4d5
Avneesh Kumar <kaviikumar010@gmail.com>
2024-06-09 12:11:27 +0530

    new

---
+++ LearnJs/Math.js
@@ -0,0 +1,34 @@
+
+/********************Math Opertor
+ 1 Addition +
+ 2 Subtraction -
+ 3 Multiplication *
+ 4 Division /
+ 5 Remainder/ Modulo %
+ 6 Exponentiation**
+********************* */
+//*****Exponetiation***** */ 
+
+// b= 15**2
+// a=2**10
+// console.log(a)
+
+let b = Math.sqrt(-9);
+console.log(b)
+
+//power function
+let a=Math.pow(2,6)
+console.log(a)
+//Math.SQRT2
+function getRoot2() {
+    return Math.SQRT2;
+  }
+  
+  console.log(getRoot2());
+
+  // Math Floor
+  let c=Math.floor(2.99)
+  console.log(c)
+  let d=Math.ceil(2.99)
+  console.log(d)
+
